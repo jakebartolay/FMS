46ae009e82b5724dedd3e7e90753c72461b2db31
Jake Bartolay <68772728+jakebartolay@users.noreply.github.com>
2024-02-21 21:39:15 +0800

    Remove Manager And Fixing Admin UI FOR VENDOR AND INVESTMENT

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -43,7 +43,8 @@
             <li class="nav-item">
                 <a class="nav-link collapsed" data-bs-target="#components-nav2" data-bs-toggle="collapse"
                     href="#">
-                    <i class="bi bi-cash-coin"></i><span>Investment Management</span><i class="bi bi-chevron-down ms-auto"></i>
+                    <i class="bi bi-cash-coin"></i><span>Investment Management</span><i
+                        class="bi bi-chevron-down ms-auto"></i>
                 </a>
                 <ul id="components-nav2" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                     <li>
@@ -82,17 +83,362 @@
                 </ol>
             </nav>
         </div><!-- End Page Title -->
-        
+
         <section class="section dashboard">
             <div class="row justify-content-center align-center d-flex">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-body pt-3">
-                            <h1>Welcome, {{ $roleName }} {{ $user->name }}</h1>
-                        </div>
+                <div class="col-lg-12">
+                    <div class="row">
+
+                        <!-- Vendor Card -->
+                        <div class="col-xxl-3 col-md-6 col-12">
+                            <div class="card info-card sales-card">
+
+                                <div class="filter">
+                                    <a class="icon" href="#" data-bs-toggle="dropdown"><i
+                                            class="bi bi-three-dots"></i></a>
+                                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                                        <li class="dropdown-header text-start">
+                                            <h6>Filter</h6>
+                                        </li>
+
+                                        <li><a class="dropdown-item" href="#">Today</a></li>
+                                        <li><a class="dropdown-item" href="#">This Month</a></li>
+                                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="card-body">
+                                    <h5 class="card-title">Vendor Account <span>| Today</span></h5>
+
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-person-fill-check"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            {{-- <h6>145</h6>
+                                            <span class="text-success small pt-1 fw-bold">12%</span> <span
+                                                class="text-muted small pt-2 ps-1">increase</span> --}}
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div><!-- End Vendor Card -->
+
+                        <!-- Vendor Card -->
+                        <div class="col-xxl-3 col-md-6 col-12">
+
+                            <div class="card info-card customers-card">
+
+                                <div class="filter">
+                                    <a class="icon" href="#" data-bs-toggle="dropdown"><i
+                                            class="bi bi-three-dots"></i></a>
+                                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                                        <li class="dropdown-header text-start">
+                                            <h6>Filter</h6>
+                                        </li>
+
+                                        <li><a class="dropdown-item" href="#">Today</a></li>
+                                        <li><a class="dropdown-item" href="#">This Month</a></li>
+                                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="card-body">
+                                    <h5 class="card-title">Active Vendor <span>| This Year</span></h5>
+
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-person-fill-dash"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            {{-- <h6>1244</h6>
+                                            <span class="text-danger small pt-1 fw-bold">12%</span> <span
+                                                class="text-muted small pt-2 ps-1">decrease</span> --}}
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div><!-- End Vendor Card -->
+
+                        <!-- Investment Card -->
+                        <div class="col-xxl-3 col-md-6 col-12">
+                            <div class="card info-card revenue-card">
+
+                                <div class="filter">
+                                    <a class="icon" href="#" data-bs-toggle="dropdown"><i
+                                            class="bi bi-three-dots"></i></a>
+                                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                                        <li class="dropdown-header text-start">
+                                            <h6>Filter</h6>
+                                        </li>
+
+                                        <li><a class="dropdown-item" href="#">Today</a></li>
+                                        <li><a class="dropdown-item" href="#">This Month</a></li>
+                                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="card-body">
+                                    <h5 class="card-title">Investment Account <span>| This Month</span></h5>
+
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-person-fill-check"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            {{-- <h6>$3,264</h6>
+                                            <span class="text-success small pt-1 fw-bold">8%</span> <span
+                                                class="text-muted small pt-2 ps-1">increase</span> --}}
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div><!-- End Investment Card -->
+
+                        <!-- Investment Card -->
+                        <div class="col-xxl-3 col-md-6 col-12">
+
+                            <div class="card info-card customers-card">
+
+                                <div class="filter">
+                                    <a class="icon" href="#" data-bs-toggle="dropdown"><i
+                                            class="bi bi-three-dots"></i></a>
+                                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                                        <li class="dropdown-header text-start">
+                                            <h6>Filter</h6>
+                                        </li>
+
+                                        <li><a class="dropdown-item" href="#">Today</a></li>
+                                        <li><a class="dropdown-item" href="#">This Month</a></li>
+                                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="card-body">
+                                    <h5 class="card-title">Active Investment <span>| This Year</span></h5>
+
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-person-fill-dash"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            {{-- <h6>1244</h6>
+                                            <span class="text-danger small pt-1 fw-bold">12%</span> <span
+                                                class="text-muted small pt-2 ps-1">decrease</span> --}}
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div><!-- End Investment Card -->
+
                     </div>
                 </div>
             </div>
+
+            <div class="pagetitle">
+                <nav>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item active">Vendor Management</li>
+                    </ol>
+                </nav>
+            </div><!-- End Page Title -->
+
+            <!-- Reports -->
+            <div class="col-12">
+                <div class="card">
+
+                    <div class="filter">
+                        <a class="icon" href="#" data-bs-toggle="dropdown"><i
+                                class="bi bi-three-dots"></i></a>
+                        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                            <li class="dropdown-header text-start">
+                                <h6>Filter</h6>
+                            </li>
+
+                            <li><a class="dropdown-item" href="#">Today</a></li>
+                            <li><a class="dropdown-item" href="#">This Month</a></li>
+                            <li><a class="dropdown-item" href="#">This Year</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="card-body">
+                        <h5 class="card-title">Reports <span>/Today</span></h5>
+
+                        <!-- Line Chart -->
+                        <div id="reportsChart"></div>
+
+                        <script>
+                            document.addEventListener("DOMContentLoaded", () => {
+                                new ApexCharts(document.querySelector("#reportsChart"), {
+                                    series: [{
+                                        name: 'Sales',
+                                        data: [31, 40, 28, 51, 42, 82, 56],
+                                    }, {
+                                        name: 'Revenue',
+                                        data: [11, 32, 45, 32, 34, 52, 41]
+                                    }, {
+                                        name: 'Customers',
+                                        data: [15, 11, 32, 18, 9, 24, 11]
+                                    }],
+                                    chart: {
+                                        height: 350,
+                                        type: 'area',
+                                        toolbar: {
+                                            show: false
+                                        },
+                                    },
+                                    markers: {
+                                        size: 4
+                                    },
+                                    colors: ['#4154f1', '#2eca6a', '#ff771d'],
+                                    fill: {
+                                        type: "gradient",
+                                        gradient: {
+                                            shadeIntensity: 1,
+                                            opacityFrom: 0.3,
+                                            opacityTo: 0.4,
+                                            stops: [0, 90, 100]
+                                        }
+                                    },
+                                    dataLabels: {
+                                        enabled: false
+                                    },
+                                    stroke: {
+                                        curve: 'smooth',
+                                        width: 2
+                                    },
+                                    xaxis: {
+                                        type: 'datetime',
+                                        categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z",
+                                            "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z",
+                                            "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z",
+                                            "2018-09-19T06:30:00.000Z"
+                                        ]
+                                    },
+                                    tooltip: {
+                                        x: {
+                                            format: 'dd/MM/yy HH:mm'
+                                        },
+                                    }
+                                }).render();
+                            });
+                        </script>
+                        <!-- End Line Chart -->
+
+                    </div>
+
+                </div>
+            </div><!-- End Reports -->
+
+
+            <div class="pagetitle">
+                <nav>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item active">Investment Management</li>
+                    </ol>
+                </nav>
+            </div><!-- End Page Title -->
+
+            <!-- Reports -->
+            <div class="col-12">
+                <div class="card">
+
+                    <div class="filter">
+                        <a class="icon" href="#" data-bs-toggle="dropdown"><i
+                                class="bi bi-three-dots"></i></a>
+                        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                            <li class="dropdown-header text-start">
+                                <h6>Filter</h6>
+                            </li>
+
+                            <li><a class="dropdown-item" href="#">Today</a></li>
+                            <li><a class="dropdown-item" href="#">This Month</a></li>
+                            <li><a class="dropdown-item" href="#">This Year</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="card-body">
+                        <h5 class="card-title">Reports <span>/Today</span></h5>
+
+                        <!-- Line Chart -->
+                        <div id="reportsChart2"></div>
+
+                        <script>
+                            document.addEventListener("DOMContentLoaded", () => {
+                                new ApexCharts(document.querySelector("#reportsChart2"), {
+                                    series: [{
+                                        name: 'Sales',
+                                        data: [31, 40, 28, 51, 42, 82, 56],
+                                    }, {
+                                        name: 'Revenue',
+                                        data: [11, 32, 45, 32, 34, 52, 41]
+                                    }, {
+                                        name: 'Customers',
+                                        data: [15, 11, 32, 18, 9, 24, 11]
+                                    }],
+                                    chart: {
+                                        height: 350,
+                                        type: 'area',
+                                        toolbar: {
+                                            show: false
+                                        },
+                                    },
+                                    markers: {
+                                        size: 4
+                                    },
+                                    colors: ['#4154f1', '#2eca6a', '#ff771d'],
+                                    fill: {
+                                        type: "gradient",
+                                        gradient: {
+                                            shadeIntensity: 1,
+                                            opacityFrom: 0.3,
+                                            opacityTo: 0.4,
+                                            stops: [0, 90, 100]
+                                        }
+                                    },
+                                    dataLabels: {
+                                        enabled: false
+                                    },
+                                    stroke: {
+                                        curve: 'smooth',
+                                        width: 2
+                                    },
+                                    xaxis: {
+                                        type: 'datetime',
+                                        categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z",
+                                            "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z",
+                                            "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z",
+                                            "2018-09-19T06:30:00.000Z"
+                                        ]
+                                    },
+                                    tooltip: {
+                                        x: {
+                                            format: 'dd/MM/yy HH:mm'
+                                        },
+                                    }
+                                }).render();
+                            });
+                        </script>
+                        <!-- End Line Chart -->
+
+                    </div>
+
+                </div>
+            </div><!-- End Reports -->
         </section>
 
     </main><!-- End #main -->
