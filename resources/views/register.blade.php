a8f271054e9f4edb5e667e4ec8edc087446dc758
Jake Bartolay <68772728+jakebartolay@users.noreply.github.com>
2024-02-25 02:32:20 +0800

    shadow login register back-end

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -20,7 +20,7 @@
                             @include('layout.logo2')
                             <br>
 
-                            <div class="card mb-3">
+                            <div class="card mb-3 shadow-lg">
 
                                 <div class="card-body">
 
