7bf7992479ab6452174d9549a0e08f730146f480
Jake Bartolay <68772728+jakebartolay@users.noreply.github.com>
2024-02-28 13:47:09 +0800

    new

--- resources/views/user/deposit/investments.blade.php
+++ resources/views/user/deposit/investments.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Investment</title>
+    <title>Dasboard</title>
     @include('layout.header')
 </head>
 
@@ -99,7 +99,7 @@
         <ul class="sidebar-nav" id="sidebar-nav">
 
             <li class="nav-item">
-                <a class="nav-link collapsed" href="/">
+                <a class="nav-link" href="/">
                     <i class="bi bi-grid-fill"></i>
                     <span>Dashboard</span>
                 </a>
@@ -120,7 +120,7 @@
             </li><!-- End Contact Page Nav -->
 
             <li class="nav-item">
-                <a class="nav-link" href="/investment">
+                <a class="nav-link collapsed" href="/investment">
                     <i class="bi bi-folder-fill"></i>
                     <span>My Investment</span>
                 </a>
@@ -153,122 +153,116 @@
                     <h1>Dashboard</h1>
                 </div>
             </div>
-            <nav>
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="/">Home</a></li>
-                    <li class="breadcrumb-item active">Investment</li>
-                </ol>
-            </nav>
+            <div class="row">
+                <div class="d-flex justify-content-between">
+                    <nav>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="/">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                    </nav>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <a class="btn btn-outline-secondary rounded-pill btn-sm me-2" href="#invest">Investment</a>
+                        <a class="btn btn-primary rounded-pill btn-sm" href="/wallet">Deposit</a>
+                    </div>
+                </div>
+            </div>
         </div><!-- End Page Title -->
 
         <section class="section dashboard">
             <div class="row">
-                <!-- Right side columns -->
-                <div class="col-lg-12 col-12">
-                    <div class="row justify-content-center">
-                        <div class="col-md-12">
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="card-title">
-                                        <div class="row justify-content-between">
-                                            <div class="col">
-                                                <h2 class="mb-4">My Investments</h2>
-                                            </div>
-                                            {{-- <div class="col-auto">
-                                                <button type="button" class="btn btn-outline-primary"
-                                                    data-bs-toggle="modal" data-bs-target="#verticalycentered">Create
-                                                    Investment</button>
-                                            </div> --}}
-                                            <hr>
-                                        </div>
-                                    </div>
-                                    <table class="table table-responsive datatable">
-                                        <thead class="table-primary">
-                                            <tr>
-                                                <th>#</th>
-                                                <th>Account</th>
-                                                <th>Amount</th>
-                                                <th>Investment Date</th>
-                                                <th>Status</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($invest as $row)
-                                                <tr>
-                                                    <th scope="row">{{ $row->id }}</th>
-                                                    <td>{{ $row->firstname }} {{ $row->lastname }}</td>
-                                                    <td>{{ number_format($row->amount, 2) }}</td>
-                                                    <td>{{ $row->created_at->format('m-d-y') }}</td>
-                                                    <td>
-                                                        @if ($row->status_name == 'Active')
-                                                            <span
-                                                                class="badge bg-success">{{ $row->status_name }}</span>
-                                                        @elseif($row->status_name == 'Inactive')
-                                                            <span class="badge bg-danger">{{ $row->status_name }}</span>
-                                                        @elseif($row->status_name == 'Pending')
-                                                            <span
-                                                                class="badge bg-warning text-dark">{{ $row->status_name }}</span>
-                                                        @elseif($row->status_name == 'Completed')
-                                                            <span
-                                                                class="badge bg-primary">{{ $row->status_name }}</span>
-                                                        @elseif($row->status_name == 'Cancelled')
-                                                            <span
-                                                                class="badge bg-secondary">{{ $row->status_name }}</span>
-                                                        @elseif($row->status_name == 'Suspended')
-                                                            <span class="badge bg-info">{{ $row->status_name }}</span>
-                                                        @elseif($row->status_name == 'Failed')
-                                                            <span class="badge bg-dark">{{ $row->status_name }}</span>
-                                                        @elseif($row->status_name == 'Refunded')
-                                                            <span
-                                                                class="badge bg-light text-dark">{{ $row->status_name }}</span>
-                                                        @elseif($row->status_name == 'Approve')
-                                                            <span
-                                                                class="badge bg-primary">{{ $row->status_name }}</span>
-                                                        @elseif($row->status_name == 'Cancel')
-                                                            <span
-                                                                class="badge bg-warning text-black">{{ $row->status_name }}</span>
-                                                        @elseif($row->status_name == 'Delete')
-                                                            <span
-                                                                class="badge bg-danger text-white">{{ $row->status_name }}</span>
-                                                        @endif
+                <!-- Left side columns -->
+                <div class="col-lg-12">
+                    <div class="row">
 
-                                                    </td>
-                                                    {{-- <td>
-                                                            <li class="list-inline-item">
-                                                                <button class="btn btn-danger btn-sm rounded"
-                                                                    type="button" data-toggle="tooltip"
-                                                                    data-placement="top" title="Delete"><i
-                                                                        class="bi bi-x-lg"></i></button>
-                                                            </li>
-                                                        </td> --}}
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
+
+                    </div><!-- End Right side columns -->
+                </div>
+                <!-- Right side columns -->
+                <div class="col-lg-12 col-12" id="invest">
+                    <!-- Website Traffic -->
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Investment</h5>
+                            <div class="row g-4">
+
+                                <div class="col-xl-12 col-md-12">
+                                    <div class="row">
+
+                                        <div class="col-xxl-3 col-md-6 col-12">
+                                            <div class="card border py-3">
+                                                <div class="card-body text-center">
+                                                    <img src="assets/img/companylogo/coca-cola-logo.png"
+                                                        class="border border-circle rounded-circle" alt="logo payment"
+                                                        width="100px" height="100px" srcset="">
+                                                    <div class="my-3">
+                                                        <span class="fs-6 text-muted py-5">Coca Cola</span>
+                                                    </div>
+                                                    <a href="#" class="btn btn-primary" data-bs-toggle="modal"
+                                                        data-bs-target="#cocola">Invest</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
                                 </div>
+
                             </div>
                         </div>
                     </div>
-                </div>
-            </div><!-- End Website Traffic -->
-            </div><!-- End Left side columns -->
-        </section>
+                </div><!-- End Website Traffic -->
+                {{-- <div class="col-lg-12">
+                    <div class="row">
 
-        {{-- <div class="modal fade" id="verticalycentered" tabindex="-1">
+                        <!-- Investment Card -->
+                        <div class="col-xxl-12 col-md-12 col-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Log History</h5>
+                                    <span class="break"></span>
+                                    <div class="container-fluid">
+                                        <table class="table table-striped datatable">
+                                            <thead>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>Actions</th>
+                                                    <th>Date</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <!-- @foreach ($activityLog as $data)
+                                                    <tr>
+                                                    <td>{{ $data->id }}</td>
+                                                    <td>{{ $data->description }}</td>
+                                                    <td>{{ $data->date_time }}</td>
+                                                </tr>
+                                                @endforeach -->
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- End Investment Card -->
+                    </div><!-- End Right side columns -->
+                </div> --}}
+            </div><!-- End Left side columns -->
+            </div>
+        </section>
+        <div class="modal fade" id="cocola" tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title">Create Investment</h5>
+                        <h5 class="modal-title">Coca Cola Investment</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal"
                             aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <form method="POST" action="{{ route('investment.invest') }}">
+                        <form method="POST" action="{{ route('InvestmentRequest.store') }}">
                             @csrf
                             <div class="form-group mb-3">
                                 <label for="amount">Amount:</label>
-                                <input id="amount" type="number" placeholder="0.00" min="0"
-                                    max="10000000" class="form-control" value="{{ old('amount') }}" required>
+                                <input id="amount" name="amount" type="number" placeholder="0.00" min="0"
+                                    max="10000000" class="form-control" required>
                                 @error('amount')
                                     <span class="invalid-feedback" role="alert">
                                         {{ $message }}
@@ -279,7 +273,7 @@
                             <div class="form-group mb-3">
                                 <label for="investment_date">Investment Date:</label>
                                 <input type="date" name="investment_date" id="investment_date"
-                                    class="form-control" value="{{ old('investment_date') }}" required>
+                                    class="form-control" required>
                                 @error('investment_date')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -287,13 +281,12 @@
                                 @enderror
                             </div>
 
-                            <button type="submit" class="btn btn-primary">Create Investment</button>
+                            <button type="submit" class="btn btn-primary">Confirm</button>
                         </form>
                     </div>
                 </div>
             </div>
-        </div> --}}
-
+        </div>
     </main><!-- End #main -->
 
     @include('layout.footer')
